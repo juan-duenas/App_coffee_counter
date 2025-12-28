b1b293707dba0e18a94740a9dac4968375110838
Juan F. Dueñas <62545646+juan-duenas@users.noreply.github.com>
2025-12-28 21:20:48 +0100

    modified main.py. Added file detection routines

--- src/main.py
+++ src/main.py
@@ -2,23 +2,22 @@ import config
 
 def main():
     print("Hello from app-coffee-counter!")
-    print(f"Sample images are located in: {config.SAMPLE_IMAGES_DIR}")
     
     # Check example images directory is found
     if config.SAMPLE_IMAGES_DIR.exists() and config.SAMPLE_IMAGES_DIR.is_dir():
-        print("Sample images found:")
+        print("\u2713 Sample images found")
     else:
         print("Sample images directory not found or is not a directory.")
 
     # Check if local model directory is present
     if config.MODELS_DIR.exists() and config.MODELS_DIR.is_dir():
-        print("local checkpoint found")
+        print("\u2713 local croppie checkpoint directory found")
     else:
-        print("local checkpoint directory not found, or is not a directory")
+        print("local croppie checkpoint directory not found, or is not a directory")
 
     # Check if yolov5 repo is downloaded locally and is in root
     if config.YOLOV5_DIR.exists() and config.YOLOV5_DIR.is_dir():
-        print("local copy of YOLO v5 repo was found")
+        print("\u2713 local copy of YOLO v5 repo was found")
     else:
         print("A local copy of YOLO v5 repo was not found in the root directory.")
 
